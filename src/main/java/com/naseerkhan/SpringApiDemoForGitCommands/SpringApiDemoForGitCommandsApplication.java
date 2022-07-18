75acb664e9da5848967d5e332160210fd899cbcb
Naseer Khan <naseerkhan200@gmail.com>
2022-07-18 13:24:02 +0530

    New spring boot project setup configured

---
+++ src/main/java/com/naseerkhan/SpringApiDemoForGitCommands/SpringApiDemoForGitCommandsApplication.java
@@ -0,0 +1,13 @@
+package com.naseerkhan.SpringApiDemoForGitCommands;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiDemoForGitCommandsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiDemoForGitCommandsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naseerkhan/SpringApiDemoForGitCommands/SpringApiDemoForGitCommandsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naseerkhan.SpringApiDemoForGitCommands;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiDemoForGitCommandsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
