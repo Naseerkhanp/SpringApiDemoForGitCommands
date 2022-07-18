a6c113b555142211ffbd4cebe47fd0c7c6ee6bfc
Naseer Khan <naseerkhan200@gmail.com>
2022-07-18 20:27:38 +0530

    updated third time

--- src/main/java/com/naseerkhan/SpringApiDemoForGitCommands/SpringApiDemoForGitCommandsApplication.java
+++ src/main/java/com/naseerkhan/SpringApiDemoForGitCommands/SpringApiDemoForGitCommandsApplication.java
@@ -9,6 +9,7 @@ public class SpringApiDemoForGitCommandsApplication {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("Naseer Khan");
+		System.out.println("Naseer Khan1");
 		SpringApplication.run(SpringApiDemoForGitCommandsApplication.class, args);
 	}
 
